6756fb96bba689a7d9870eedeae6cb5c7adcc913
Rodriguetech <rodriguet802@gmail.com>
2021-08-28 02:16:03 +0100

    Trajet entity

---
+++ src/Entity/Trajet.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrajetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TrajetRepository::class)
+ */
+class Trajet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $depart;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $destination;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $heurDep;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $heurDes;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDepart(): ?string
+    {
+        return $this->depart;
+    }
+
+    public function setDepart(string $depart): self
+    {
+        $this->depart = $depart;
+
+        return $this;
+    }
+
+    public function getDestination(): ?string
+    {
+        return $this->destination;
+    }
+
+    public function setDestination(string $destination): self
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+
+    public function getHeurDep(): ?\DateTimeInterface
+    {
+        return $this->heurDep;
+    }
+
+    public function setHeurDep(\DateTimeInterface $heurDep): self
+    {
+        $this->heurDep = $heurDep;
+
+        return $this;
+    }
+
+    public function getHeurDes(): ?\DateTimeInterface
+    {
+        return $this->heurDes;
+    }
+
+    public function setHeurDes(\DateTimeInterface $heurDes): self
+    {
+        $this->heurDes = $heurDes;
+
+        return $this;
+    }
+
+    
+}
